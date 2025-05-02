0ca1e2503d64da9bb24cc102a368694c3bb40573
Lucas Fontgalland Pandori <Lucas Pandori>
2025-05-02 18:33:38 -0300

    listar com filtro feito

--- back-end/src/controladores/Listar.js
+++ back-end/src/controladores/Listar.js
@@ -1,12 +1,21 @@
 import pool from "../conexaoBD";
 
 export async function ListarAnimais(req, res) {
+    const { filtro } = req.query
     const cliente = await pool.connect()
 
     try {
-        const listar = await cliente.query('SELECT * FROM pet')
 
-        return res.status(200).json(listar.rows)
+        if (!filtro) {
+            const listar = await cliente.query('SELECT * FROM pet')
+
+            return res.status(200).json(listar.rows)
+        }
+
+        const listarFiltro = await cliente.query('SELECT * FROM pet WHERE nome = $1 OR idade = $2 OR cor = $3 OR microchipado = $4 OR especie = $5', [filtro])
+
+        return res.status(200).json(listarFiltro.rows)
+
 
     } catch (error) {
         console.error(error)
--- back-end/src/rotas.js
+++ back-end/src/rotas.js
@@ -1,9 +1,11 @@
 import { Router } from "express"
 import { CadastrarAnimal } from "./controladores/CriarAnimal"
+import { ListarAnimais } from "./controladores/Listar"
 
 const rotas = Router()
 
 rotas.post('/cadastro', CadastrarAnimal)
+rotas.get('/listar', ListarAnimais)
 
 
 export default rotas
