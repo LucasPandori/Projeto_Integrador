9773b42c8ab3ca2c6e5c603deb09d698b5f4f811
Lucas Fontgalland Pandori <Lucas Pandori>
2025-05-15 16:26:10 -0300

    back-end feito

--- back-end/src/controladores/Listar.js
+++ back-end/src/controladores/Listar.js
@@ -2,7 +2,7 @@ import pool from "../conexaoBD.js";
 
 export async function ListarAnimais(req, res) {
     try {
-        const [rows] = await pool.query('SELECT * FROM pets')
+        const [rows] = await pool.query('SELECT nome, especie, raça, idade, cor, microchipado, tutor, endereço, email, observações, foto, telefone FROM pets')
 
         return res.status(200).json(rows)
 
